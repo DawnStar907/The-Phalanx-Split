18b8d4c072da87c8e99ee7f31f826ea14c4c0acf
DawnStar <76675443+DawnStar907@users.noreply.github.com>
2022-11-17 22:09:09 -0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+//project started at 9:40 Thu, Nov 17 2022
+
+#include <stdio.h>
+char name[30];
+
+int main(void) {
+  printf(" Sirens sound, as you grogily get up you hear the rush of boots and the rumble from SPAV armored vehicles rolling from above. You get up and read the report from your jumplink device, it reads <Kulashna seperatist forces have infiltrated   the capitol all soldiers must report for a code 5 mission objective to be assigned, this is not a drill.>");
+  printf("\n\n\nyou wear a look of terror as you realise that your training was not without a reason, you get up, get in uniform, and report to your squadron leader\n\n\n");
+  printf("Choose a code name"); 
+  return 0;
+}
